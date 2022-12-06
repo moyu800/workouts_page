8960932f240dbd8df8224e42336b0ab50137fe51
moyu800 <42370847+moyu800@users.noreply.github.com>
2022-12-06 20:52:03 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,18 +2,18 @@ module.exports = {
   pathPrefix: '/workouts_page', // Change to `/workouts_page` when running on GitHub pages
   siteMetadata: {
     siteTitle: 'Workouts Map',
-    siteUrl: 'https://ben29.xyz',
+    siteUrl: 'https://moyu935.xyz',
     logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
     description: 'Personal Workouts Page',
     keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://ben29.xyz',
+        url: 'https://moyu935.xyz',
       },
       {
         name: 'About',
-        url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+        url: 'https://github.com/moyu935/workouts_page/blob/master/README-CN.md',
       },
     ],
   },
